5176003a769757b895e5ae0ec7fae828c802d9a1
alvihossain1 <2020780@iub.edu.bd>
2023-12-13 11:06:41 +0600

    7th commit

--- script.js
+++ script.js
@@ -9,43 +9,45 @@ function processWeather(data){
     console.log(data)
     var content2 = document.getElementById("content-weather");
     content2.innerHTML = `
-    <div class="mt-1">
-        <p>Search Results for: <span class="highlight-1">${data.name}</span></p>
-    </div>
-    <div class="d-flex flex-wrap mt-3 gap-3">
-        <div class="small-box">
-            <h5 class="my-0 light-text">Weather</h5>
-            <p class="my-0 highlight-2">${data.weather[0].main}</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Temperature</h5>
-            <p class="my-0 highlight-2">${data.main.temp}°C</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Feels Like</h5>
-            <p class="my-0 highlight-2">${data.main.feels_like}°C</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Humidity</h5>
-            <p class="my-0 highlight-2">${data.main.humidity}%</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Pressure</h5>
-            <p class="my-0 highlight-2">${data.main.pressure}</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Visibility</h5>
-            <p class="my-0 highlight-2">${data.visibility}</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0">Min Temp</h5>
-            <p class="my-0 highlight-2">${data.main.temp_min}°C</p>
-        </div>
-        <div class="small-box">
-            <h5 class="my-0 light-text">Max Temp</h5>
-            <p class="my-0 highlight-2">${data.main.temp_max}°C</p>
-        </div>                
-    </div>
+            <div class="box-2">
+                <div class="content-head">
+                    <p>Search Results for: <span class="highlight-1">${data.name}</span></p>
+                </div>
+                <div class="small-box-holder">
+                    <div class="small-box">
+                        <h4 class="light-text">Weather</h4>
+                        <p class="highlight-2">${data.weather[0].main}</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Temperature</h4>
+                        <p class="highlight-2">${data.main.temp}°C</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Feels Like</h4>
+                        <p class="highlight-2">${data.main.feels_like}°C</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Humidity</h4>
+                        <p class="highlight-2">${data.main.humidity}%</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Pressure</h4>
+                        <p class="highlight-2">${data.main.pressure}</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Visibility</h4>
+                        <p class="highlight-2">${data.visibility}</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Min Temp</h4>
+                        <p class="highlight-2">${data.main.temp_min}°C</p>
+                    </div>
+                    <div class="small-box">
+                        <h4 class="light-text">Max Temp</h4>
+                        <p class="highlight-2">${data.main.temp_max}°C</p>
+                    </div>                
+                </div>
+            </div>
     
     `;
 }
